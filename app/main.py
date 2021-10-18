a5e9676f317c049d5ef6891626116fb6c72234e1
Han Wang <hyw2@cornell.edu>
2021-10-18 15:47:38 -0400

    Update location

--- app/main.py
+++ app/main.py
@@ -4,6 +4,10 @@ from guesslang import Guess
 guess = Guess()
 
 app= Flask(__name__)
+@app.route('/')
+def index():
+  return "Welcome to Infercode by Figstack"
+
 @app.route('/<code>')
 def infer(code):
   language = guess.language_name(code)
