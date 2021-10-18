2f932bd723803f983fec39b47b9ca421ba69208f
Han Wang <hyw2@cornell.edu>
2021-10-18 13:47:05 -0400

    Add setup for guesslang

--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 from flask import Flask
-# from guesslang import Guess
+from guesslang import Guess
 
-# guess = Guess()
+guess = Guess()
 
 app= Flask(__name__)
 @app.route('/<code>')
 def infer(code):
-  return code
+  language = guess.language_name(code)
+  return language
